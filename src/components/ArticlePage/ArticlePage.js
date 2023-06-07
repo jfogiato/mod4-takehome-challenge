9e978f4968b23057206d3536c7272639052298e5
Joe Fogiato <joe.fogiato@gmail.com>
2023-06-07 12:17:04 -0400

    Add: padding to page and additional checks for author status

--- src/components/ArticlePage/ArticlePage.js
+++ src/components/ArticlePage/ArticlePage.js
@@ -6,13 +6,13 @@ const ArticlePage = ({ article }) => {
   const cleanedContent = content.split('[')[0];
 
   return (
-    <div>
+    <div className='p-3'>
       <h1 className='text-center'>{title}</h1>
       <div className='container-sm'>
         <img src={urlToImage} alt={title} className='img-fluid mh-10' />
       </div>
-      {(author !== 'Staff Writer' || !author) && <h2>Written by {author}</h2>}
-      <h3 className='font-italic'>{date}</h3>
+      {(author !== 'Staff Writer' && author && !author.includes('https')) && <h2>Written by {author}</h2>}
+      <h3 className='font-italic'>{date}</h3><br/>
       <p>{cleanedContent}</p>
       <a href={url} target='blank' >Read More {`->`}</a>
     </div>
