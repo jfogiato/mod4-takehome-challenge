2f74f7bce041af4714603a40c5446f1a470b8841
Joe Fogiato <joe.fogiato@gmail.com>
2023-06-07 12:57:57 -0400

    Update: home page and tile styling

--- src/components/ArticleTile/ArticleTile.js
+++ src/components/ArticleTile/ArticleTile.js
@@ -6,12 +6,12 @@ const ArticleTile = ({ articleData }) => {
   const date = new Date(publishedAt).toString().split(' ').slice(0, 4).join(' ');
 
   return (
-    <div className="card m-3" style={{width: "25vw", height: "40vh"}}>
+    <div className="card m-3" style={{width: "35vmin", height: "auto", maxHeight: "60vmin"}}>
       <Link to={`articles/${id}`}><img className="card-img-top" src={urlToImage} alt={title}/></Link>
-      <div className="card-body">
+      <div className="card-body d-inline-block d-flex flex-column justify-center align-center">
         <Link to={`articles/${id}`}><h6 className="card-title text-truncate">{title}</h6></Link>
-        <p className="card-text text-truncate">{description}</p>
-        <p className="card-text text-truncate">{date}</p>
+        <p className="card-text h-40 text-truncate">{description}</p>
+        <p className="card-text">{date}</p>
       </div>
     </div>
   );
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -14,7 +14,7 @@ const NavBar = ({ searchArticles }) => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+    <nav className="navbar bg-body-tertiary sticky-top d-flex">
       <div className="container-fluid">
         <Link to="/"><p className="navbar-brand">NewsLinkz</p></Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,19 +22,19 @@ const NavBar = ({ searchArticles }) => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
+            <form className="d-flex" role="search" onSubmit={handleSearch}>
+              <input 
+                className="form-control me-2" 
+                type="search" 
+                placeholder="Search" 
+                value={searchTerm} 
+                onChange={handleChange}
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
           </div>
         </div>
-        <form className="d-flex" role="search" onSubmit={handleSearch}>
-          <input 
-            className="form-control me-2" 
-            type="search" 
-            placeholder="Search" 
-            value={searchTerm} 
-            onChange={handleChange}
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </nav>
   );
